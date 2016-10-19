657e6c43d2f64255782f4dbb229cad6d61fde0f2
songzb <szbhust@gmail.com>
2016-10-18 20:52:27 -0500

    All completed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+       @all_ratings = ['G', 'PG', 'PG-13', 'R']
+
+   def self.put_ratings 
+        @all_ratings
+    end
+      def self.ratingfilter(rating)
+        Movie.where('rating in (?)', rating)
+    end
 end
